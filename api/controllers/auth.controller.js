4ffc270a9fb0a67d1a14a6fca95c198faa2940a4
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-10-30 15:50:29 +0530

    Create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res,next) => {
     const { username, email, password } = req.body;
@@ -27,4 +28,32 @@ export const signup = async (req, res,next) => {
 
 
 
-}
+};
+
+export const signin = async (req, res,next) => {
+    const {email, password} = req.body;
+    if (!email || !password || email === "" || password === "") {
+        next(errorHandler(400, "All fields are required"));
+    }
+    try {
+        const validuser = await User.findOne({ email });
+        if (!validuser) {
+           return next(errorHandler(404, "User not found"));
+        }
+        const validPassword = bcryptjs.compareSync(password, validuser.password);
+        if (!validPassword) {
+          return  next(errorHandler(401, "Invalid password"));
+        }
+        const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET
+        );
+        const { password: pass, ...rest } = validuser._doc;
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+            
+        }).json(rest);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+};
