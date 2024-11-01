654bf995a7f93f4cb506a5ab008a774c0fe56d2c
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-11-01 21:28:45 +0530

    slug is working

--- client/src/Pages/PostPage.jsx
+++ client/src/Pages/PostPage.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export default function PostPage() {
+    const { postSlug } = useParams();
+
+    useEffect(() => {
+        console.log(postSlug)
+    }, [postSlug]);
   return (
     <div>PostPage</div>
   )
