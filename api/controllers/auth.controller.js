a86b33841941caba84de8d535f88d4a7e7f9c401
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-11-01 10:50:39 +0530

    create admin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,7 +44,7 @@ export const signin = async (req, res,next) => {
         if (!validPassword) {
           return  next(errorHandler(401, "Invalid password"));
         }
-        const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET
+        const token = jwt.sign({ id: validuser._id, isAdmin:validuser.isAdmin }, process.env.JWT_SECRET
         );
         const { password: pass, ...rest } = validuser._doc;
         res.status(200).cookie('access_token', token, {
@@ -64,7 +64,7 @@ export const google = async (req, res, next) => {
       const user = await User.findOne({ email });
       if (user) {
         const token = jwt.sign(
-          { id: user._id },
+          { id: user._id, isAdmin:user.isAdmin },
           process.env.JWT_SECRET
         );
         const { password, ...rest } = user._doc;
@@ -89,7 +89,7 @@ export const google = async (req, res, next) => {
         });
         await newUser.save();
         const token = jwt.sign(
-          { id: newUser._id },
+          { id: newUser._id, isAdmin:newUser.isAdmin },
           process.env.JWT_SECRET
         );
         const { password, ...rest } = newUser._doc;
