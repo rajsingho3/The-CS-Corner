248fc198f36730eddd0fe32efa00a0c0dc3ed847
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-11-01 21:36:44 +0530

    call to action completed

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Button } from 'flowbite-react';
 
 export default function CallToAction() {
   return (
-    <div>CallToAction</div>
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className="flex-1 justify-center flex flex-col">
+            <h2 className='text-2xl'>
+                Want to learn more about MERN Stack Development?
+            </h2>
+            <p className='text-gray-500 my-2'>
+                Use for Promoting pages or important things
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
+                    Something you want to promote
+                </a>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+        </div>
+    </div>
   )
 }
