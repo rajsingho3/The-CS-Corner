d71795b6a9c9005d09f7c21b224379d262dfbe90
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-11-01 21:24:43 +0530

    post page started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './Pages/CreatePost'
 import UpdatePost from './Pages/UpdatePost'
+import PostPage from './Pages/PostPage'
 
 
 export default function App() {
@@ -32,6 +33,7 @@ export default function App() {
         </Route>
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<Project />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
 
         
       </Routes>
