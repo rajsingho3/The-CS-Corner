b165b2cfe21b4958d3879a29179da7fe29bbcc51
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-11-02 08:25:06 +0530

    dash and footer updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,40 +1,75 @@
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
-import React from 'react';
-
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub,  } from 'react-icons/bs';
 export default function FooterCom() {
   return (
-    <Footer container className='border-t-8 border-teal-500'>
-        <div>
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='mt-5'>
+            <Link
+              to='/'
+              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+            >
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-indigo-500 to-pink-500 rounded-lg text-white'>
+              Engineering
+            </span>
+            Reference
+            </Link>
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
             <div>
-                <div className=''>
-                <Link to='/'
-                 className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-indigo-500 to-pink-500 rounded-lg text-white'>
-          Engineering
-        </span>
-        Reference
-      </Link>
-                </div>
-                <div className='grid grid-cols-2 gap-3 sm: mt-4 sm:grid-cols-3 sm:gap-6'>
-                    <Footer.Title title='About' />
-                    <Footer.LinkGroup col>
-                        <Footer.Link
-                            href='/about'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                            >
-                                100js 
-                        </Footer.Link>
-                    </Footer.LinkGroup>
-
-                </div>
-                <Footer.Divider />
-                <div className=''>
-                    <Footer.Copyright href='#' by='Engineering Reference' year={new Date().getFullYear()}/>
-                </div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Engineering Reference
+                </Footer.Link>
+              </Footer.LinkGroup>
             </div>
+            <div>
+              <Footer.Title title='Follow us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://github.com/rajsingho3'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href='#'>LinkedIn</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>Privacy Policy</Footer.Link>
+                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
         </div>
-        </Footer>
-  )
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="Engineering Reference"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='#' icon={BsFacebook}/>
+            <Footer.Icon href='#' icon={BsInstagram}/>
+            <Footer.Icon href='#' icon={BsTwitter}/>
+            <Footer.Icon href='https://github.com/rajsingho3' icon={BsGithub}/>
+            
+
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
 }
