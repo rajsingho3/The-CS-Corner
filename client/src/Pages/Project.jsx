fe085a4285709917878b8aff14a2fc7726a90da0
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-11-02 08:07:58 +0530

    project page completed

--- client/src/Pages/Project.jsx
+++ client/src/Pages/Project.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
 
-export default function project() {
+
+export default function Projects() {
   return (
-    <div>project</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'> Stay tuned for upcoming project uploads</p>
+      
+    </div>
   )
 }
