2cff6851ef64df8d6c7975349babf6b5817d2cde
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-11-02 19:50:12 +0530

    hadle link home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route,} from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import Home from './Pages/Home.jsx'
 import Signin from './Pages/Signin.jsx'
 import Signup from './Pages/Signup'
 import Dashboard from './pages/Dashboard'
