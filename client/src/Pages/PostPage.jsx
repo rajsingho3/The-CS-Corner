4f497906b5b1e32bae1a8f9161eddae8e57b3812
RAJ SINGH <142092924+rajsingho3@users.noreply.github.com>
2024-11-02 09:45:51 +0530

    improve post page

--- client/src/Pages/PostPage.jsx
+++ client/src/Pages/PostPage.jsx
@@ -70,11 +70,13 @@ export default function PostPage() {
           {post && post.category}
         </Button>
       </Link>
-      <img
-        src={post && post.image}
-        alt={post && post.title}
-        className='mt-10 p-3 max-h-[600px] w-full object-cover'
-      />
+      <a href={post && post.image} target="_blank" rel="noopener noreferrer">
+        <img
+          src={post && post.image}
+          alt={post && post.title}
+          className='mt-10 p-3 max-h-[500px] w-full object-contain' // Ensure full image is visible
+        />
+      </a>
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>
@@ -85,11 +87,7 @@ export default function PostPage() {
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      <div className='max-w-4xl mx-auto w-full'>
-        <CallToAction />
-      </div>
-      <CommentSection postId={post._id} />
-
+      
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
